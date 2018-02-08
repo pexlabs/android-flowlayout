266f2f5062acb307c7852dc5c05eba1349d7115d
Kaustubh <kaustubh.deshmukh27@gmail.com>
2018-02-08 11:29:43 +0530

    Fixed email validation logic
    - Now email accepts special characters like "&, *, +, =, /, $, {, }"

--- libraries/layouts/src/main/java/org/apmem/tools/util/Utils.java
+++ libraries/layouts/src/main/java/org/apmem/tools/util/Utils.java
@@ -1,15 +1,20 @@
 package org.apmem.tools.util;
 
-import android.util.Patterns;
-
 /**
  * Created by kaustubh on 27/10/17.
  */
 
 public final class Utils {
 
+    /**
+     * Referenced from <a href="https://www.regextester.com/19" />
+     */
+    public static final String EMAIL_ADDRESS = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
+            "@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,64}[a-zA-Z0-9])?" +
+            "(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,25}[a-zA-Z0-9])?)*$";
+
     public static boolean isValidEmailAddress(String email) {
-        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        return email.matches(EMAIL_ADDRESS);
     }
 
 }
